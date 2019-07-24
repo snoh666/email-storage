a60df3ca8d036a9f619b81fc08f2d9376352b568
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-24 19:34:30 +0200

    Basic Router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from './components/Login';
+import EmailList from './components/EmilList';
+import NewEmail from './components/NewEmail';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Email storage</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Login} />
+        <Route path="/list" component={EmailList} />
+        <Route path='/new-email' component={NewEmail} />
+      </div>
+    </Router>
   );
 }
 
