8c614106650c7dfac0722681cb971181fba4e624
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-24 20:34:41 +0200

    Added useReducer functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { useReducer } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Login from './components/Login';
-import EmailList from './components/EmilList';
-import NewEmail from './components/NewEmail';
+import ItemList from './components/ItemList';
+import NewItem from './components/NewItem';
+import reducer, { initialState } from './Functions';
+
 
 function App() {
+
+  let [state, dispatch] = useReducer(reducer, initialState());
+  console.log(state);
+
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        <Route path="/list" component={EmailList} />
-        <Route path='/new-email' component={NewEmail} />
+        <Route exact path="/" component={({history}) => <Login history={history} dispatch={dispatch} propPassword={state.password !== null ? state.password : false}/>} />
+        <Route path="/list" component={() => <ItemList />} />
+        <Route path='/new-item' component={() => <NewItem />} />
       </div>
     </Router>
   );
