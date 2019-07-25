1efb1f0e3b1c9404510271e19f3904d910d9272b
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-25 14:55:34 +0200

    Added history prop to NewItem

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="App">
         <Route exact path="/" component={({history}) => <Login history={history} dispatch={dispatch} propPassword={state.password !== null ? state.password : false}/>} />
         <Route path="/list" component={() => <ItemList items={state.list.length > 0 ? state.list : false} />} />
-        <Route path='/new-item' component={() => <NewItem dispatch={dispatch}/>} />
+        <Route path='/new-item' component={({ history }) => <NewItem history={history} dispatch={dispatch}/>} />
       </div>
     </Router>
   );
