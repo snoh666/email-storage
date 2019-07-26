bc8efe143574424bd450c350d929175ac18881c4
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-26 10:38:50 +0200

    Updates

---
+++ precache-manifest.ca04502db926fdb69b72fc1fe4093d97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b30a52baa4f1366db1a88251739324d",
+    "url": "/email-storage/index.html"
+  },
+  {
+    "revision": "c1a9f86c0fd5b087c232",
+    "url": "/email-storage/static/css/main.56f774ac.chunk.css"
+  },
+  {
+    "revision": "3c678ea4d45b864837a7",
+    "url": "/email-storage/static/js/2.0e99b853.chunk.js"
+  },
+  {
+    "revision": "c1a9f86c0fd5b087c232",
+    "url": "/email-storage/static/js/main.514ad7c4.chunk.js"
+  },
+  {
+    "revision": "7a705e83d4cfed87a8c8",
+    "url": "/email-storage/static/js/runtime~main.423dfd34.js"
+  }
+]);
