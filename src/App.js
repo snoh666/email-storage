c072f83c182cae22c6cfe491f26b5f1fc995033d
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-26 10:48:58 +0200

    Added basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   let [state, dispatch] = useReducer(reducer, initialState());
 
   return (
-    <Router>
+    <Router basename="/email-storage/">
       <div className="App">
         <Route exact path="/" component={({history}) => <Login history={history} dispatch={dispatch} propPassword={state.password !== null ? state.password : false}/>} />
         <Route path="/list" component={() => <ItemList items={state.list.length > 0 ? state.list : false} />} />
