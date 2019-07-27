5f1f0706bd6ed8c0d38e7ced2b15ecf810286329
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-27 08:25:40 +0200

    Added dispatch in props

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <Router basename="/email-storage/">
       <div className="App">
         <Route exact path="/" component={({history}) => <Login history={history} dispatch={dispatch} propPassword={state.password !== null ? state.password : false}/>} />
-        <Route path="/list" component={() => <ItemList items={state.list.length > 0 ? state.list : false} />} />
+        <Route path="/list" component={() => <ItemList dispatch={dispatch} items={state.list.length > 0 ? state.list : false} />} />
         <Route path='/new-item' component={({ history }) => <NewItem history={history} dispatch={dispatch}/>} />
       </div>
     </Router>
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import SiteItem from './SiteItem'
 
-function ItemList({ items }) {
+function ItemList({ items, dispatch }) {
 
   return (
     <div className="List">
@@ -12,7 +12,7 @@ function ItemList({ items }) {
       </Link>
       <div className="list-container">
         {
-          items !== false ? items.map(x => (<SiteItem key={x.id} value={x} />)) : <span className="info">No items inputed</span>
+          items !== false ? items.map(x => (<SiteItem key={x.id} value={x} dispatch={dispatch} />)) : <span className="info">No items inputed</span>
         }
       </div>
     </div>
