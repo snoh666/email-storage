95d38771e711b8a89ae35d27fe57d012291e8d71
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-27 10:57:31 +0200

    Deleted history prop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useReducer } from 'react';
 import { HashRouter as Router, Route} from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import Login from './components/Login';
 import ItemList from './components/ItemList';
 import NewItem from './components/NewItem';
@@ -10,10 +9,9 @@ import reducer, { initialState } from './Functions';
 function App() {
 
   let [state, dispatch] = useReducer(reducer, initialState());
-  const history = createBrowserHistory();
 
   return (
-    <Router basename="/email-storage/" history={history} >
+    <Router basename="/email-storage/" >
       <div className="App">
         <Route exact path="/" component={({history}) => <Login history={history} dispatch={dispatch} propPassword={state.password !== null ? state.password : false}/>} />
         <Route path="/list" component={() => <ItemList dispatch={dispatch} items={state.list.length > 0 ? state.list : false} />} />
