f4c73123d113dfa6c4a2434b77922677553a1744
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-27 11:12:05 +0200

    Fixed hyperlink

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -7,9 +7,11 @@ function ItemList({ items, dispatch }) {
   return (
     <div className="List">
       <h1>Item list</h1>
-      <Link to='/new-item'>
-        <button>New Item</button>
-      </Link>
+      <div>
+        <Link to='/new-item'>
+          New Item
+        </Link>
+      </div>
       <div className="list-container">
         {
           items !== false ? items.map(x => (<SiteItem key={x.id} value={x} dispatch={dispatch} />)) : <span className="info">No items inputed</span>
