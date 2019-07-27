b3a2cf485fa8b99517ff6ef49cfc5ab669a901b2
Mikolaj Snochowski <snochopl@gmail.com>
2019-07-27 08:26:57 +0200

    Added deleteItem function with dispatch from prop

--- src/components/SiteItem.jsx
+++ src/components/SiteItem.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 
-function SiteItem({ value }) {
+function SiteItem({ value, dispatch }) {
 
   const { siteName, username, password } = value;
+
+  const deleteItem = () => {
+    console.log(value);
+
+    dispatch({ type: 'DELETE_ITEM', body: { id: value.id}});
+    console.log('Should be deleted');
+  }
 
   return (
     <div className="list-item">
       <span>Site name: {siteName} </span>
       <span> Username: {username} </span>
       <span> Password: {password} </span>
+      <button className="delete" onClick={deleteItem}>X</button>
     </div>
   );
 }
